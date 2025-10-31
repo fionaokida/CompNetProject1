3c506b89055c8419259c5113e6487742024a6045
fionaokida <105687577+fionaokida@users.noreply.github.com>
2025-10-31 03:17:27 -0700

    pasted the client code on accident. should be fixed now

--- pt2/udp_server_fionaokida_919165599_Ian.py
+++ pt2/udp_server_fionaokida_919165599_Ian.py
@@ -1,22 +1,32 @@
 import socket
+import time
 
-SERVER_HOST = '127.0.0.1'
-SERVER_PORT = 5500
+HOST = '127.0.0.1'
+PORT = 5500
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
+    server_socket.bind((HOST, PORT))
 
-    bytes_left = 100 * 1024 * 1024  # 100 MB
-    packet = b'.' * 1024 # packets are 1 KB
+    bytes_received = 0
+    while True:
+        if bytes_received == 0:
+            tStart = time.time()
+        
+        data, addr = server_socket.recvfrom(1024)
 
-    while bytes_left > 0:
-        client_socket.sendto(packet, (SERVER_HOST, SERVER_PORT))
-        bytes_left -= len(packet)
+        bytes_received += len(data) # increment by number of bytes received
 
-    client_socket.sendto(b"DONE", (SERVER_HOST, SERVER_PORT))
+        message = data.decode()
+        if message == "DONE":
+            tEnd = time.time()
 
-    response, _ = client_socket.recvfrom(1024)
+            totTime = tEnd - tStart
 
-    kBps = response.decode()
-    print(f"{kBps}")
+            throughput = bytes_received / totTime
+            kBps = throughput / 1024 # 1 kilobyte is 1024 bytes? 1000 bytes?
 
-    client_socket.close()
+            response = f"Throughput is {kBps} kB/s"
+            server_socket.sendto(response.encode(), addr)
+
+            print(f"Total bytes: {bytes_received} bytes Total time: {totTime} seconds")
+            bytes_received = 0
